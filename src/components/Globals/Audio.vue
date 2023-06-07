24a3ddeb44843f87a4e6f4058eaed5614afe25fa
Aaron_Zhou <aaron_zhou@digifinex.org>
2023-06-07 13:07:44 +0800

    fix: fix audio status event problems

--- src/components/Globals/Audio.vue
+++ src/components/Globals/Audio.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { useAudioStore } from 'stores'
+import { useAudioStore, usePlaylistStore } from 'stores'
 
-const { audioRef, src, loop } = storeToRefs(useAudioStore())
+const { audioRef, loop } = storeToRefs(useAudioStore())
 const { updateAudioStatus, updateCurrentTime, updateDuration } = useAudioStore()
+const { currentSong } = storeToRefs(usePlaylistStore())
 </script>
 
 <template>
     <audio
         ref="audioRef"
-        :src="src"
+        :src="currentSong?.url"
         :loop="loop"
-        @canplay="(e) => updateDuration((e.target as HTMLAudioElement).duration)"
+        @loadedmetadata="updateAudioStatus('not-ready')"
+        @canplay="(e) => {
+            updateDuration((e.target as HTMLAudioElement).duration)
+            updateAudioStatus('can-play')
+        }"
         @durationchange="(e) => updateDuration((e.target as HTMLAudioElement).duration)"
         @timeupdate="(e) => updateCurrentTime((e.target as HTMLAudioElement).currentTime)"
         @play="updateAudioStatus('playing')"
--- src/components/Globals/index.ts
+++ src/components/Globals/index.ts
@@ -1,2 +1,3 @@
+export { default as Audio } from './Audio.vue'
 export { default as LoginModal } from './LoginModal/LoginModal.vue'
 export { default as Nav } from './Nav/Nav.vue'
