0437367ccb79ad18d713c3a0346eb79af47b43d7
Aaron_Zhou <aaron_zhou@digifinex.org>
2023-06-09 15:49:58 +0800

    fix: fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        base: 'music',
         envDir: './env',
         define: {
             __VUE_OPTIONS_API__: false
