68e559e92099d0dcdc402463440382fe0811f3f0
Aaron_Zhou <clark1729@outlook.com>
2023-06-11 17:01:12 +0800

    feat: global scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,32 @@ usePlayStatusEffect()
     <Audio />
 </template>
 
-<style></style>
+<style lang="scss">
+* {
+    // Firefox
+    scrollbar-width: thin;
+
+    &::-webkit-scrollbar {
+        width: 8px;
+        height: 4px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        position: absolute;
+        top: 0;
+        right: 0;
+        min-height: 20px;
+        border-radius: 8px;
+        background-clip: content-box;
+        box-shadow: 0 0 0 8px hsl(var(--b2)) inset;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        box-shadow: 0 0 0 8px hsl(var(--b3)) inset;
+    }
+
+    &::-webkit-scrollbar-corner {
+        background: transparent;
+    }
+}
+</style>
