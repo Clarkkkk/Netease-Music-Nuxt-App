e5d01f71d77200f42f886c0bdc3118a7bed4246c
Aaron_Zhou <clark1729@outlook.com>
2023-07-28 21:34:01 +0800

    fix: fix a bug that `collapsible` prop is not working properly and improve the strategy to guess the max height more acurately

--- src/components/Collapsible.vue
+++ src/components/Collapsible.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-import { ref, useSlots } from 'vue'
+import { computed, ref, useSlots, watch } from 'vue'
 import { Button } from 'components'
+import { wait } from '../utils/wait'
 
 const slots = useSlots()
 
-defineProps<{
+const props = defineProps<{
     collapseHeight: number
     collapsible?: boolean
+    possibleMaxHeight?: number
 }>()
 
 const isOpen = ref(false)
+const container = ref<HTMLElement | null>(null)
+const memorizedHeight = ref(props.possibleMaxHeight || 2000)
+
+const height = computed(() => {
+    if (!props.collapsible) {
+        return 'auto'
+    } else if (isOpen.value) {
+        return `${memorizedHeight.value}px`
+    } else {
+        return `${props.collapseHeight}px`
+    }
+})
+
+watch(isOpen, async (val) => {
+    if (val && container.value) {
+        await wait(800)
+        memorizedHeight.value = container.value.clientHeight
+    }
+})
 </script>
 
 <template>
     <div class="relative pb-6">
         <div
-            class="overflow-hidden transition-all"
+            ref="container"
+            class="overflow-hidden transition-all duration-500"
             :style="{
-                maxHeight: isOpen ? '2000px' : `${collapseHeight}px`
+                maxHeight: height
             }"
         >
             <slot name="default"></slot>
         </div>
 
         <Button
-            class="btn-ghost btn-xs absolute bottom-0"
+            class="btn-ghost btn-xs absolute -bottom-2 h-8 px-0"
             @click="isOpen = !isOpen"
         >
             <slot
@@ -32,7 +54,9 @@ const isOpen = ref(false)
                 name="button-content"
                 :is-open="isOpen"
             />
-            <span v-if="!slots['button-content']">{{ isOpen ? '收起' : '展开' }}</span>
+            <span v-if="collapsible && !slots['button-content']">
+                {{ isOpen ? '收起' : '展开' }}
+            </span>
         </Button>
     </div>
 </template>
