ad8c264b52f335bc5b7146f47d9642bc95d82baf
Aaron_Zhou <clark1729@outlook.com>
2023-09-27 16:01:05 +0800

    chore: page router init

---
+++ pages/index/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div
+        id="home"
+        class="container mx-auto mt-2 flex flex-col items-center pt-16 lg:mt-6 lg:flex-row lg:items-start lg:justify-between"
+    >
+        hello
+    </div>
+</template>
+
+<style>
+.new-songs:not(:first-child) {
+    @apply mt-12;
+}
+</style>
