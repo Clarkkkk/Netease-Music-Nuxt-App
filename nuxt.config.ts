229e67d474024d372c0a4e8d536264704ea4043e
Aaron_Zhou <clark1729@outlook.com>
2023-09-27 16:55:59 +0800

    chore: nuxt alias and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
+import { fileURLToPath } from 'url'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true }
+    devtools: { enabled: true },
+    components: [{ path: '~/components/globals', pathPrefix: false }, '~/components'],
+    modules: ['@pinia/nuxt', 'unplugin-icons/nuxt'],
+    alias: {
+        common: fileURLToPath(new URL('./common', import.meta.url)),
+        services: fileURLToPath(new URL('./services', import.meta.url)),
+        api: fileURLToPath(new URL('./api', import.meta.url)),
+        utils: fileURLToPath(new URL('./utils', import.meta.url)),
+        stores: fileURLToPath(new URL('./stores', import.meta.url))
+    }
 })
