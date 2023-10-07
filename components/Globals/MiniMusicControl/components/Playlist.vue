12f492e4cde9b4254ee2c71ed8bb38a836051e89
Aaron_Zhou <clark1729@outlook.com>
2023-10-07 17:51:56 +0800

    fix: fix inconsistency between ssr and csr

--- components/Globals/MiniMusicControl/components/Playlist.vue
+++ components/Globals/MiniMusicControl/components/Playlist.vue
@@ -11,62 +11,64 @@ const { switchToThisSong, removeSong } = usePlaylistStore()
 
 <template>
     <div>
-        <ContextMenu
-            placement="top-end"
-            size="large"
-        >
-            <template #default>
-                <Button
-                    v-view-transition-name="'playing-playlist-btn'"
-                    class="btn-ghost btn-square btn-sm ml-3 p-0"
-                >
-                    <i-solar-plaaylist-minimalistic-bold-duotone class="h-5 w-5" />
-                </Button>
-            </template>
+        <ClientOnly>
+            <ContextMenu
+                placement="top-end"
+                size="large"
+            >
+                <template #default>
+                    <Button
+                        v-view-transition-name="'playing-playlist-btn'"
+                        class="btn-ghost btn-square btn-sm ml-3 p-0"
+                    >
+                        <i-solar-plaaylist-minimalistic-bold-duotone class="h-5 w-5" />
+                    </Button>
+                </template>
 
-            <template #menu>
-                <ul class="playlist-menu">
-                    <ContextMenuItem
-                        v-for="song in playlist"
-                        :key="song.id"
-                        class="flex flex-nowrap items-center overflow-hidden whitespace-nowrap"
-                        :active="song === currentSong"
-                        @click="switchToThisSong(song)"
-                    >
-                        <i-solar-play-bold
-                            v-if="song === currentSong"
-                            class="h-3 w-3 flex-fixed overflow-hidden text-primary"
-                        />
-                        <span
-                            class="overflow-hidden text-ellipsis text-sm"
-                            :title="song.name"
+                <template #menu>
+                    <ul class="playlist-menu">
+                        <ContextMenuItem
+                            v-for="song in playlist"
+                            :key="song.id"
+                            class="flex flex-nowrap items-center overflow-hidden whitespace-nowrap"
+                            :active="song === currentSong"
+                            @click="switchToThisSong(song)"
                         >
-                            {{ song.name }}
-                        </span>
-                        <span
-                            class="flex-auto overflow-hidden text-ellipsis text-left text-xs text-base-content/60"
-                            :title="song.artist"
+                            <i-solar-play-bold
+                                v-if="song === currentSong"
+                                class="h-3 w-3 flex-fixed overflow-hidden text-primary"
+                            />
+                            <span
+                                class="overflow-hidden text-ellipsis text-sm"
+                                :title="song.name"
+                            >
+                                {{ song.name }}
+                            </span>
+                            <span
+                                class="flex-auto overflow-hidden text-ellipsis text-left text-xs text-base-content/60"
+                                :title="song.artist"
+                            >
+                                {{ song.artist }}
+                            </span>
+                            <Button
+                                class="btn-ghost btn-square btn-sm"
+                                @click.stop="removeSong(song)"
+                            >
+                                <template #icon>
+                                    <i-fluent-dismiss-32-regular class="h-3 w-3" />
+                                </template>
+                            </Button>
+                        </ContextMenuItem>
+                        <div
+                            v-if="!playlist.length"
+                            class="my-2 flex h-full w-full items-center justify-center text-sm text-base-content/50"
                         >
-                            {{ song.artist }}
-                        </span>
-                        <Button
-                            class="btn-ghost btn-square btn-sm"
-                            @click.stop="removeSong(song)"
-                        >
-                            <template #icon>
-                                <i-fluent-dismiss-32-regular class="h-3 w-3" />
-                            </template>
-                        </Button>
-                    </ContextMenuItem>
-                    <div
-                        v-if="!playlist.length"
-                        class="my-2 flex h-full w-full items-center justify-center text-sm text-base-content/50"
-                    >
-                        - 列表为空 -
-                    </div>
-                </ul>
-            </template>
-        </ContextMenu>
+                            - 列表为空 -
+                        </div>
+                    </ul>
+                </template>
+            </ContextMenu>
+        </ClientOnly>
     </div>
 </template>
 
