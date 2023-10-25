d38493370f8db84c51c7d9223c1d81030f7c54db
Aaron_Zhou <clark1729@outlook.com>
2023-10-25 14:26:51 +0800

    chore: cleanup gitignore

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "../../node_modules/.pnpm/nuxt@3.7.4_@types+node@20.7.0_eslint@8.42.0_rollup@2.79.1_sass@1.62.1_typescript@5.1.6_vue-tsc@1.8.11/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
